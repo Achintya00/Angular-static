346293f3b071c2b8d79a0c31e7fdd80a96b123c1
Achintya00 <55224555+Achintya00@users.noreply.github.com>
2023-03-05 01:52:11 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    theme: {
+      screens: {
+        sm: "500px",
+        md: "800px",
+        lg: "1200",
+        xl: "1440",
+      },
+    },
   },
   plugins: [],
 };
